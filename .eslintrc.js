14d2180a3519be0a9c668c4dacc230224ed06aea
Anton Zykov <anton.zykov1@gmail.com>
2023-05-15 16:47:32 +0300

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true,
+        "jest": true
+    },
+    "extends": ["airbnb"],
+    "parserOptions": {
+        "ecmaVersion": 14
+    },
+    "rules": {
+        'no-unused-vars': 0,
+        'no-console': 0,
+        'import/order': 0,
+        'import/newline-after-import': 0,
+        'object-curly-newline': 0,
+        'comma-dangle': 0,
+        'no-underscore-dangle': 0
+    }
+};
